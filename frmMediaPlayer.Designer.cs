2ed53d93333e6db5a82fe1ffc066f4f12b992e7c
ChihYu0831 <chyulola434@gmail.com>
2026-05-03 21:16:03 +0800

    新增影片速度調整功能（快轉/慢動作/正常）
    - 於 UI 增加影片速度選擇區（0.5x、1x、2x）
    - 播放前需先選擇速度，播放時速度不可變更
    - 停止後可重新選擇速度
    - 調整控制按鈕排列與視窗大小
    - 修正檔案篩選器格式
    - 重構速度選擇啟用邏輯，減少重複程式碼

--- frmMediaPlayer.Designer.cs
+++ frmMediaPlayer.Designer.cs
@@ -30,26 +30,79 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMediaPlayer));
             this.palButton = new System.Windows.Forms.Panel();
-            this.wmpVideo = new AxWMPLib.AxWindowsMediaPlayer();
-            this.btnBrowser = new System.Windows.Forms.Button();
-            this.btnPlay = new System.Windows.Forms.Button();
-            this.btnPause = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.wmpVideo = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnFastForward = new System.Windows.Forms.Button();
+            this.btnSlowMotion = new System.Windows.Forms.Button();
+            this.grpAdjustSpeed = new System.Windows.Forms.GroupBox();
+            this.btnNormalSpeed = new System.Windows.Forms.Button();
             this.palButton.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).BeginInit();
+            this.grpAdjustSpeed.SuspendLayout();
             this.SuspendLayout();
             // 
             // palButton
             // 
+            this.palButton.Controls.Add(this.grpAdjustSpeed);
             this.palButton.Controls.Add(this.btnStop);
             this.palButton.Controls.Add(this.btnPause);
             this.palButton.Controls.Add(this.btnPlay);
             this.palButton.Controls.Add(this.btnBrowser);
             this.palButton.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.palButton.Location = new System.Drawing.Point(0, 243);
+            this.palButton.Location = new System.Drawing.Point(0, 253);
             this.palButton.Name = "palButton";
-            this.palButton.Size = new System.Drawing.Size(568, 75);
+            this.palButton.Size = new System.Drawing.Size(644, 198);
             this.palButton.TabIndex = 1;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStop.Location = new System.Drawing.Point(395, 19);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 42);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Enabled = false;
+            this.btnPause.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPause.Location = new System.Drawing.Point(275, 19);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 42);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "暫停";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Enabled = false;
+            this.btnPlay.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPlay.Location = new System.Drawing.Point(155, 19);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 42);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = "播放";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBrowser.Location = new System.Drawing.Point(35, 19);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 42);
+            this.btnBrowser.TabIndex = 2;
+            this.btnBrowser.Text = "瀏覽";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
             // 
             // wmpVideo
             // 
@@ -58,61 +111,63 @@
             this.wmpVideo.Location = new System.Drawing.Point(0, 0);
             this.wmpVideo.Name = "wmpVideo";
             this.wmpVideo.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpVideo.OcxState")));
-            this.wmpVideo.Size = new System.Drawing.Size(568, 318);
+            this.wmpVideo.Size = new System.Drawing.Size(644, 451);
             this.wmpVideo.TabIndex = 0;
             // 
-            // btnBrowser
+            // btnFastForward
             // 
-            this.btnBrowser.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnBrowser.Location = new System.Drawing.Point(27, 18);
-            this.btnBrowser.Name = "btnBrowser";
-            this.btnBrowser.Size = new System.Drawing.Size(75, 42);
-            this.btnBrowser.TabIndex = 2;
-            this.btnBrowser.Text = "瀏覽";
-            this.btnBrowser.UseVisualStyleBackColor = true;
-            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            this.btnFastForward.Enabled = false;
+            this.btnFastForward.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnFastForward.Location = new System.Drawing.Point(428, 30);
+            this.btnFastForward.Name = "btnFastForward";
+            this.btnFastForward.Size = new System.Drawing.Size(92, 42);
+            this.btnFastForward.TabIndex = 6;
+            this.btnFastForward.Text = "快轉 (2x)";
+            this.btnFastForward.UseVisualStyleBackColor = true;
+            this.btnFastForward.Click += new System.EventHandler(this.btnFastForward_Click);
             // 
-            // btnPlay
+            // btnSlowMotion
             // 
-            this.btnPlay.Enabled = false;
-            this.btnPlay.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnPlay.Location = new System.Drawing.Point(121, 18);
-            this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(75, 42);
-            this.btnPlay.TabIndex = 3;
-            this.btnPlay.Text = "播放";
-            this.btnPlay.UseVisualStyleBackColor = true;
-            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            this.btnSlowMotion.Enabled = false;
+            this.btnSlowMotion.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSlowMotion.Location = new System.Drawing.Point(41, 30);
+            this.btnSlowMotion.Name = "btnSlowMotion";
+            this.btnSlowMotion.Size = new System.Drawing.Size(117, 42);
+            this.btnSlowMotion.TabIndex = 7;
+            this.btnSlowMotion.Text = "慢動作 (0.5x)";
+            this.btnSlowMotion.UseVisualStyleBackColor = true;
+            this.btnSlowMotion.Click += new System.EventHandler(this.btnSlowMotion_Click);
             // 
-            // btnPause
+            // grpAdjustSpeed
             // 
-            this.btnPause.Enabled = false;
-            this.btnPause.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnPause.Location = new System.Drawing.Point(216, 18);
-            this.btnPause.Name = "btnPause";
-            this.btnPause.Size = new System.Drawing.Size(75, 42);
-            this.btnPause.TabIndex = 4;
-            this.btnPause.Text = "暫停";
-            this.btnPause.UseVisualStyleBackColor = true;
-            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            this.grpAdjustSpeed.Controls.Add(this.btnNormalSpeed);
+            this.grpAdjustSpeed.Controls.Add(this.btnSlowMotion);
+            this.grpAdjustSpeed.Controls.Add(this.btnFastForward);
+            this.grpAdjustSpeed.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpAdjustSpeed.Location = new System.Drawing.Point(35, 82);
+            this.grpAdjustSpeed.Name = "grpAdjustSpeed";
+            this.grpAdjustSpeed.Size = new System.Drawing.Size(564, 90);
+            this.grpAdjustSpeed.TabIndex = 8;
+            this.grpAdjustSpeed.TabStop = false;
+            this.grpAdjustSpeed.Text = "影片速度";
             // 
-            // btnStop
+            // btnNormalSpeed
             // 
-            this.btnStop.Enabled = false;
-            this.btnStop.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnStop.Location = new System.Drawing.Point(311, 18);
-            this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(75, 42);
-            this.btnStop.TabIndex = 5;
-            this.btnStop.Text = "停止";
-            this.btnStop.UseVisualStyleBackColor = true;
-            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            this.btnNormalSpeed.Enabled = false;
+            this.btnNormalSpeed.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNormalSpeed.Location = new System.Drawing.Point(232, 30);
+            this.btnNormalSpeed.Name = "btnNormalSpeed";
+            this.btnNormalSpeed.Size = new System.Drawing.Size(122, 42);
+            this.btnNormalSpeed.TabIndex = 8;
+            this.btnNormalSpeed.Text = "正常速度 (1x)";
+            this.btnNormalSpeed.UseVisualStyleBackColor = true;
+            this.btnNormalSpeed.Click += new System.EventHandler(this.btnNormalSpeed_Click);
             // 
             // frmMediaPlayer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(568, 318);
+            this.ClientSize = new System.Drawing.Size(644, 451);
             this.Controls.Add(this.palButton);
             this.Controls.Add(this.wmpVideo);
             this.Name = "frmMediaPlayer";
@@ -120,6 +175,7 @@
             this.Text = "多媒體播放器";
             this.palButton.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).EndInit();
+            this.grpAdjustSpeed.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -132,6 +188,10 @@
         private System.Windows.Forms.Button btnPlay;
         private System.Windows.Forms.Button btnBrowser;
         private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSlowMotion;
+        private System.Windows.Forms.Button btnFastForward;
+        private System.Windows.Forms.GroupBox grpAdjustSpeed;
+        private System.Windows.Forms.Button btnNormalSpeed;
     }
 }
 
--- frmMediaPlayer.cs
+++ frmMediaPlayer.cs
@@ -12,6 +12,9 @@ namespace MediaPlayer
 {
     public partial class frmMediaPlayer : Form
     {
+        // 1. 新增一個變數，用來記錄使用者選擇的速度
+        private double _selectedSpeed = 1.0;
+
         public frmMediaPlayer()
         {
             InitializeComponent();
@@ -20,32 +23,100 @@ namespace MediaPlayer
         private void btnBrowser_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "WMV files (*.wmv)|*.wmv|MP4files(*.mp4) | *.mp4 | AVI files(*.avi) | *.avi | All files(*.*) | *.* ";
+            ofd.Filter = "WMV files (*.wmv)|*.wmv|MP4 files (*.mp4)|*.mp4|AVI files (*.avi)|*.avi|All files (*.*)|*.*";
+
             if (ofd.ShowDialog() == DialogResult.OK)
             {
+                wmpVideo.settings.autoStart = false;
                 wmpVideo.URL = ofd.FileName;
-                wmpVideo.Ctlcontrols.stop(); // 停止
 
-                btnPlay.Enabled = true;
-                btnPause.Enabled = true;
-                btnStop.Enabled = true;
+                // 載入影片後，可以選速度
+                btnFastForward.Enabled = true;
+                btnNormalSpeed.Enabled = true;
+                btnSlowMotion.Enabled = true;
+
+                // 還沒選速度，所以播放不能按
+                btnPlay.Enabled = false;
+
+                // 還沒播放，所以暫停和停止都不能按
+                btnPause.Enabled = false;
+                btnStop.Enabled = false;
 
             }
         }
 
+        // --- 速度選擇按鈕事件 ---
+        private void btnFastForward_Click(object sender, EventArgs e)
+        {
+            _selectedSpeed = 2.0;
+            EnablePlayButton();
+        }
+
+        private void btnNormalSpeed_Click(object sender, EventArgs e)
+        {
+            _selectedSpeed = 1.0;
+            EnablePlayButton();
+        }
+
+        private void btnSlowMotion_Click(object sender, EventArgs e)
+        {
+            _selectedSpeed = 0.5;
+            EnablePlayButton();
+        }
+
+        // 用來把播放按鈕打開的共用方法
+        private void EnablePlayButton()
+        {
+            btnPlay.Enabled = true;
+        }
+
+        // --- 播放按鈕事件 ---
         private void btnPlay_Click(object sender, EventArgs e)
         {
-            wmpVideo.Ctlcontrols.play(); // 播放
+            wmpVideo.settings.rate = _selectedSpeed;
+            wmpVideo.Ctlcontrols.play();
+
+            // 播放中不能再選速度
+            btnPlay.Enabled = false;
+            btnFastForward.Enabled = false;
+            btnNormalSpeed.Enabled = false;
+            btnSlowMotion.Enabled = false;
+
+            // 播放中可以暫停、停止
+            btnPause.Enabled = true;
+            btnStop.Enabled = true;
         }
 
         private void btnPause_Click(object sender, EventArgs e)
         {
-            wmpVideo.Ctlcontrols.pause(); // 暫停
+            wmpVideo.Ctlcontrols.pause();
+
+            // 暫停後可以再按播放繼續
+            btnPlay.Enabled = true;
+            btnPause.Enabled = false;
+            btnStop.Enabled = true;
+
+            // 暫停時先不讓改速度，避免邏輯混亂
+            btnFastForward.Enabled = false;
+            btnNormalSpeed.Enabled = false;
+            btnSlowMotion.Enabled = false;
         }
 
         private void btnStop_Click(object sender, EventArgs e)
         {
-            wmpVideo.Ctlcontrols.stop(); // 停止
+            wmpVideo.Ctlcontrols.stop();
+
+            // 停止後，讓使用者重新選擇速度
+            btnFastForward.Enabled = true;
+            btnNormalSpeed.Enabled = true;
+            btnSlowMotion.Enabled = true;
+
+            // 重新選速度前，播放鍵先關掉
+            btnPlay.Enabled = false;
+
+            // 停止後不能暫停、停止
+            btnPause.Enabled = false;
+            btnStop.Enabled = false;
         }
     }
 }
